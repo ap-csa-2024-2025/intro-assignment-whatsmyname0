2145ee7cd3a933e8b8c1f977ea18fd58ab0ae7ac
whatsmyname0 <mikepham2007@gmail.com>
2024-09-04 09:50:44 -0700

    Prnitnng

---
+++ Printing 1.java
@@ -0,0 +1,21 @@
+public class Printing
+{
+    public static void main(String[] args)
+    {
+        //int, double, boolean, String
+        int myAge = 999; //int is for whole numbers
+        double price = 4.99; //double is for decimal numbers
+
+
+        boolean isGood; // variable declaration
+        isGood = false; // variable initialization
+
+
+        String name = "Mike Pham"; // set name to "Mike  Pham"
+
+
+        System.out.println("Hello my name is " + name);
+        System.out.print("I'm worth ");
+        System.out.print(price);
+    }
+}
