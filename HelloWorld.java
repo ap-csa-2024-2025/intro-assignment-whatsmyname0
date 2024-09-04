13cf733863371ccea84f9d0d13a1f65725362cad
whatsmyname0 <mikepham2007@gmail.com>
2024-09-04 10:13:44 -0700

    HelloWorld1

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        // 1.1 HOMEWORK
+
+        // Problem 1
+        System.out.println("                                                                                                                                                     Cristiano Ronaldo");
+       
+       // Problem 2
+        System.out.println("Coding is fun!");   
+        System.out.println("Coding is fun!");
+        System.out.println("Coding is fun!");
+
+        //Problem 3
+        System.out.println("========="); 
+        System.out.println("= 0   0 =   Java is Awesome!");
+        System.out.println("=   v   =");
+        System.out.print("=========");
+
+        // 1.2 HOMEWORK
+
+        // Problem 1
+        String FavFood = "Steak";
+        System.out.print("I like to eat " + FavFood + " as well!");
+
+        // Problem 2
+        String name1 = "Sandy";
+        String name2 = "David";
+
+
+    }
+}
+
