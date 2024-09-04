cbb331bd004fe9d752838da64e34107f85aa8c22
whatsmyname0 <mikepham2007@gmail.com>
2024-09-04 10:18:49 -0700

    HelloWorld2

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,17 +17,26 @@ public class HelloWorld
         System.out.println("========="); 
         System.out.println("= 0   0 =   Java is Awesome!");
         System.out.println("=   v   =");
-        System.out.print("=========");
+        System.out.println("=========");
 
         // 1.2 HOMEWORK
 
         // Problem 1
         String FavFood = "Steak";
-        System.out.print("I like to eat " + FavFood + " as well!");
+        System.out.println("I like to eat " + FavFood + " as well!");
 
         // Problem 2
         String name1 = "Sandy";
         String name2 = "David";
+        String name3 = "Elaine";
+        String name4 = "Jesse";
+        System.out.println(name4 + name2 + name3 + name1);
+
+        // Problem 3
+        String name = "Mike Pham";
+        String state = "California";
+        System.out.println("My name is " + name + ". I live in " + state + ".");
+
 
 
     }
